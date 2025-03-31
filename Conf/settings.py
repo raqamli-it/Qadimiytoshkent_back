93e52800a503e9de1de2aaa7fc4c8f8fe74f2723
raqamli-it <raqamlilaboratoriya@gmail.com>
2025-03-31 14:45:16 +0500

    edit csrf

--- Conf/settings.py
+++ Conf/settings.py
@@ -76,12 +76,12 @@ MIDDLEWARE = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://subdomain.eskitoshkent.uz',
-    # 'https://subdomain.eskitoshkent.uz'
+    'http://subdomain.qadimiytoshkent.uz',
+    # 'https://subdomain.qadimiytoshkent.uz'
 ]
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_DOMAIN = 'subdomain.eskitoshkent.uz'
+CSRF_COOKIE_DOMAIN = 'subdomain.qadimiytoshkent.uz'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -96,8 +96,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "http://127.0.0.1:8017",
 #     "http://localhost:8017",
-#     "http://subdomain.eskitoshkent.uz",
-#     "https://subdomain.eskitoshkent.uz",
+#     "http://subdomain.qadimiytoshkent.uz",
+#     "https://subdomain.qadimiytoshkent.uz",
 # ]
 
 GOOGLE_REDIRECT_URL = 'https://example.com/accounts/google/login/callback/'  # or your actual callback URL
